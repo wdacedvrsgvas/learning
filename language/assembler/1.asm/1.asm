d9baf092e60ce0123adde805f44d4808cd841e99
wdacedvrsgvas <2021382145@qq.com>
2024-05-10 11:43:45 +0800

    hb

--- language/assembler/1.asm/1.asm
+++ language/assembler/1.asm/1.asm
@@ -69,3 +69,48 @@ start:
 
 code ends
 end start
+
+;不同的寻址方式的灵活应用
+;将datasg段中每个单词改为大写字母
+assume cs:codesg,ds:datasg,ss:stacksg
+
+datasg segment 
+    db 'ibm             '    ;16
+    db 'dec             '
+    db 'dos             '
+    db 'vas             '  ;看成二维数组
+datasg ends
+
+stacksg segment ;定义一个段，用来做栈段，容量为16个字节
+    dw 0,0,0,0,0,0,0,0
+stacksg ends
+
+codesg segment
+    start:  mov ax,stacksg
+            mov ss,ax
+            mov sp,16
+            mov ax,datasg
+            mov ds,ax
+            mov bx,0   ;初始ds:bx
+
+            ;cx为默认循环计数器，二重循环只有一个计数器，所以外层循环先保存cx值，再恢复，我们采用栈保存
+            mov cx,4
+    s0:     push cx   ;将外层循环的cx值入栈
+            mov si,0
+            mov cx,3
+    s:      mov al,[bx+si]
+            and al,11011111b ;每个字符转为大写字母
+            mov [bx+si],al
+            inc si
+            loop s
+
+            add bx,16 ;下一行
+            pop cx   ;恢复cx值
+            loop s0  ;外层循环的loop指令将cx中的计数值减1
+
+            mov ax,4c00h
+            int 21H
+
+codesg ends
+end start
+
